3f9b45294f11a511e4f8b4b4e25869eec64ebff0
Mokheon Park <mokimokheonpark@gmail.com>
2023-05-14 18:55:39 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import firebase from "./firebase";
+console.log(firebase);
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <App/>
-    </React.StrictMode>,
-    document.getElementById("root")
+    </React.StrictMode>
 );
