31a8e219541143ec3b0635cc8cfa904b0bed910f
Mokheon Park <mokimokheonpark@gmail.com>
2023-05-18 18:33:37 -0400

    handled Authentication

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,24 +1,32 @@
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Navigation from "components/Navigation";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
-    <Router>
+    <HashRouter>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
         )}
       </Switch>
-    </Router>
+    </HashRouter>
   );
 };
 
