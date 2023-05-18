89445f1246614753ce83ba140a3fc38ba3c64bc0
Mokheon Park <mokimokheonpark@gmail.com>
2023-05-17 21:18:16 -0400

    fourth commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-    return (
-        <>
-            <AppRouter isLoggedIn={isLoggedIn}/>
-            <footer>&copy; {new Date().getFullYear()} Twitter-Clone</footer>
-        </>
-    );
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; {new Date().getFullYear()} Twitter-Clone</footer>
+    </>
+  );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,47 +1,95 @@
 import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { authService } from "fbase";
 
 const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const onChange = (event) => {
-        const {target: {name, value}} = event;
-        if (name === "email") {
-            setEmail(value)
-        } else if (name === "password") {
-            setPassword(value);
-        }
-    };
-    const onSubmit = (event) => {
-        event.preventDefault();
-    };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+      } else {
+        data = await signInWithEmailAndPassword(authService, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    await signInWithPopup(authService, provider);
+  };
 
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    name = "email"
-                    type="eamil"
-                    placeholder="Email"
-                    value={email}
-                    onChange={onChange}
-                    required
-                />
-                <input
-                    name = "password"
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={onChange}
-                    required
-                />
-                <input type="submit" value="Log In"/>
-            </form>
-            <div>
-                <button>Continue with Goggle</button>
-                <button>Continue with GitHub</button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="eamil"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </span>
+      <div>
+        <button onClick={onSocialClick} name="google">
+          Continue with Goggle
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with GitHub
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
